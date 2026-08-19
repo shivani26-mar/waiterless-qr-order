2ea1e25283e98b0b35c6cc4060161b4db352cd68
shivani26-mar <shivii9238@gmail.com>
2026-08-19 15:47:21 +0530

    Update qr_hotel.py

--- qr_hotel.py
+++ qr_hotel.py
@@ -249,24 +249,21 @@ if view_mode == "Customer Menu (ग्राहक)":
              
             # 📌 बटन 1: केवल ऑर्डर को किचन में भेजने के लिए (स्टेटस = 'Ordered')
              if st.button("Please order 👨‍🍳", key="only_order_btn_new"):
-                  order_data = {
-                     "table_no": str(table_no),
-                     "items": str(order_summary_text),
-                     "total": int(total_bill),
-                     "status": "ordered"
-                  }
-        
-                 try:
-                     response = supabase.table("orders").insert(order_data).execute()
-                     st.write(response)
-                     st.success("🎉 ऑर्डर किचन में भेज दिया गया है!")
-        
-                 except Exception as e:
-                     st.error("❌ Order insert failed")
-                     st.write(str(e))
-        
-                  st.write("---")
-        
+                order_data = {
+                    "table_no": str(table_no),
+                    "items": str(order_summary_text),
+                    "total": int(total_bill),
+                    "status": "ordered"
+                }
+            
+                response = supabase.table("orders").insert(order_data).execute()
+            
+                st.success(
+                    f"🎉 ऑर्डर टेबल नंबर {table_no} से सीधे किचन में भेज दिया गया है!"
+                )
+            
+                st.write("---")
+                
     
 
 
